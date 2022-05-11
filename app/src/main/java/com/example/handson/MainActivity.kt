d1c4ea6c422258179c68d87eeb1dc6ee00be2bcd
U3mura <cxm.uemura12@gmail.com>
2022-05-11 20:48:03 -0300

    Menus e fragmentos

--- app/src/main/java/com/example/handson/MainActivity.kt
+++ app/src/main/java/com/example/handson/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.example.handson
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.handson.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.tutoriais -> {
+                    val frag = TutoriaisFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.container, frag).commit()
+                }
+                R.id.salvos -> {
+                    val frag = SalvosFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.container, frag).commit()
+                }
+                R.id.faq -> {
+                    val frag = FaqFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.container, frag).commit()
+                }
+//                R.id.perfil -> {
+//                    val frag = SalvosFragment()
+//                    supportFragmentManager.beginTransaction().replace(R.id.container, frag).commit()
+//                }
+
+            }
+            true
+        }
     }
 }
