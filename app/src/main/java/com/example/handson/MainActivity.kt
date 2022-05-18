4da0a3b53e70bde0f2ce8821f95d73eb8c241ea4
U3mura <cxm.uemura12@gmail.com>
2022-05-18 20:48:40 -0300

    Tela de login

--- app/src/main/java/com/example/handson/MainActivity.kt
+++ app/src/main/java/com/example/handson/MainActivity.kt
@@ -1,18 +1,27 @@
 package com.example.handson
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.handson.databinding.ActivityMainBinding
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+    lateinit var database: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        tratarLogin()
 
         binding.bottomNavigation.setOnItemSelectedListener {
             when (it.itemId) {
@@ -37,4 +46,46 @@ class MainActivity : AppCompatActivity() {
             true
         }
     }
+
+    fun tratarLogin(){
+        if (FirebaseAuth.getInstance().currentUser != null) {
+            Toast.makeText(this, "Entrou", Toast.LENGTH_SHORT).show()
+            configurarBase()
+        } else{
+            val providers = arrayListOf(
+                AuthUI.IdpConfig.EmailBuilder().build())
+
+            val i = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build()
+
+            startActivityForResult(i, 1)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 1 && resultCode == RESULT_OK){
+            Toast.makeText(this, "Autenticado", Toast.LENGTH_SHORT).show()
+            configurarBase()
+        } else {
+            finishAffinity()
+        }
+    }
+
+    fun configurarBase(){
+
+        val usuario = FirebaseAuth.getInstance().currentUser
+
+        if(usuario != null){
+
+            database = FirebaseDatabase.getInstance()
+                .reference.child(usuario.uid)
+
+        }
+
+    }
+
 }
